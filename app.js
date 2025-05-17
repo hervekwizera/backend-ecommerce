0e48bf9363ff03fbe181fa04923046dad552799c
hervekwizera <hervekwizera63@gmail.com>
2025-05-17 12:57:16 +0200

    .

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+ res.send('welcome to kigali hot market api');
+})
+
+app.listen(3000, ()=>{
+    console.log('kigali hot market is running on http://localhost:3000');
+})
+
+ export default app;
 
