4f1f63ab6dc22d66f6ed2a2f90b824d106bb7bf5
hervekwizera <hervekwizera63@gmail.com>
2025-05-19 13:03:42 +0200

    importing userRouter and authRouter in app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import {PORT} from './config/env.js';
+import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
 
 const app = express();
 
