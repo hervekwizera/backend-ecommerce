88615a7d93ee5c8ebe146905480fa96376df73e1
hervekwizera <hervekwizera63@gmail.com>
2025-06-10 23:14:01 +0200

    fixing typo in user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,7 +7,7 @@ const userRouter = Router();
 userRouter.get('/',getUsers)
 userRouter.get('/:id',authorize,getUser)
 userRouter.post('/',(req,res) => res.send({title:'CREATE new user'}))
-userRouter.put('/;id',(req,res) => res.send({title:'UPDATE user'}))
+userRouter.put('/id',(req,res) => res.send({title:'UPDATE user'}))
 userRouter.delete('/:id',(req,res) => res.send({title:'DELETE user'}));
 
 export default userRouter;
